b9400f5462b42f406f4dd5059ce7313c92a873ee
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2021-01-11 04:14:23 -0600

    Update Form1.cs

--- SocketCliente/SocketCliente/Form1.cs
+++ SocketCliente/SocketCliente/Form1.cs
@@ -12,62 +12,50 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SocketCliente
-{
-    public partial class Form1 : Form
-    {
+namespace SocketCliente{
+    public partial class Form1 : Form{
+        /*objetos a usar*/
         private TcpClient client;
         private static NetworkStream stream;
         private static StreamWriter streamw;
         private static StreamReader streamr;
-        
-
-
-
-        public Form1()
-        {
+        /*inicializamos formulario*/
+        public Form1(){
             InitializeComponent();
         }
 
-        private void enviar_Click(object sender, EventArgs e)
-        {
-            try
-            {
+        /*accion del boton para enviar mensaje al servidor*/
+        private void enviar_Click(object sender, EventArgs e){
+            try{
                 TcpClient client = new TcpClient(ip.Text, Convert.ToInt32(puerto.Text));
-                
-
+                //conexion y envio de mensaje
                 stream = client.GetStream();
                 stream.ReadTimeout = 2000;
                 streamw = new StreamWriter(stream);
                 streamr = new StreamReader(stream);
                 streamw.WriteLine(textocliente.Text);
                 streamw.Flush();
-
+                //recepcion de mensaje del servidor
                 string cliente, servidor;
                 cliente = Environment.NewLine+"CLIENTE: "+textocliente.Text;
                 servidor = Environment.NewLine + "SERVIDOR: " + streamr.ReadLine() + Environment.NewLine;
                 mensaje.Text = mensaje.Text + cliente + servidor;
                 
+                //esto es util si el servidor responde mas de una linea de codigo
                 /*string data = streamr.ReadLine();
                 while (data != null) {
                     MessageBox.Show(data);
                     data = streamr.ReadLine();
                 }*/
-                   
+                
+                //cerramos la conexion
                 stream.Close();
                 client.Close();
-                    
-                
-                
             }
             catch{
                 mensaje.Text = "Conflicto en Puerto o IP";
             }
         }
-
-
-
-
 
 
     }
